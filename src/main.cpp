db547bd13065f3dbb1f6b0f69cdcef854dfbaca5
Poyso <costantini039@gmail.com>
2025-05-25 10:56:33 +0200

    Figured out basic movement, now trying to implement collision

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,52 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <iostream>
 
+float pos_y = 0;
+float pos_x = 0;
 int main() {
   auto window =
       sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-  window.setFramerateLimit(144);
-
+  sf::RectangleShape player({100, 100});
+  sf::RectangleShape block({100, 100});
+  sf::CircleShape dot(1);
+  block.setFillColor(sf::Color::Red);
+  block.setPosition({500, 500});
+  player.setFillColor(sf::Color::Blue);
+  player.setPosition({pos_x, pos_y});
+  dot.setFillColor(sf::Color::White);
+  dot.setPosition({block.getPosition().x, block.getPosition().y + 100.f});
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>()) {
         window.close();
       }
     }
+    sf::FloatRect player_bounds = player.getGlobalBounds();
+    sf::FloatRect block_bounds = block.getGlobalBounds();
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+      pos_x++;
+      player.setPosition({pos_x, pos_y});
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+      pos_y++;
+      player.setPosition({pos_x, pos_y});
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+      pos_y--;
+      player.setPosition({pos_x, pos_y});
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) &&
+                   (!player_bounds.contains(
+                       {block.getPosition().x, block.getPosition().y + 100})) ||
+               !player_bounds.contains(block.getPosition())) {
+      pos_x--;
+      player.setPosition({pos_x, pos_y});
+    }
 
     window.clear();
+    window.draw(player);
+    window.draw(block);
+    window.draw(dot);
     window.display();
   }
 }
